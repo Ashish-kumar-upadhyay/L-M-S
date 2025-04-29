ba626c9ec2cd77b9633a7dfbb1d9fe7668847906
Ashish-kumar-upadhyay <ashishkumarupadhyay0328@gmail.com>
2025-04-29 22:56:27 +0530

    A

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://l-m-s-ashish11.vercel.app/api';
 
 // Create axios instance
 const api = axios.create({
