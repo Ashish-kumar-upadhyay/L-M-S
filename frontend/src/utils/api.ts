e10149c89dac28c3eda7668bad009ef252825b13
Ashish-kumar-upadhyay <ashishkumarupadhyay0328@gmail.com>
2025-04-30 12:23:46 +0530

    updated frntnd

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://l-m-s-1-lh3t.onrender.com/';
+const API_URL = 'https://l-m-s-1-bmg6.onrender.com/api';
 
 // Create axios instance
 const api = axios.create({
